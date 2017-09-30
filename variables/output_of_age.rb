d32ff6a4577453b8443404aca9d89c0c6bc0c61e
Marisa Ramon <mer0522@gmail.com>
2017-09-29 18:19:47 -0700

    Add 2 new chapters

---
+++ variables/output_of_age.rb
@@ -0,0 +1,10 @@
+puts "How old are you?"
+years = gets.chomp.to_i
+puts "In 10 years, you will be: "
+puts years + 10
+puts "In 20 years, you will be: "
+puts years + 20
+puts "In 30 years, you will be: "
+puts years + 30
+puts "In 40 years, you will be: "
+puts years + 40
